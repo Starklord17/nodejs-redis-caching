777ec92268cd4dfef0af1ca301fd8b2470cfeed6
Max_S <maxi.sm117@gmail.com>
2022-09-17 01:46:34 -0300

    :zap: First commit

---
+++ README.md
@@ -0,0 +1,13 @@
+### Redis Node Caching
+
+This is a basic example of how to use Redis with nodejs as a caching system to improve response speed to the client.
+
+### Requirements
+
+You need to have redis installed on your system, or other possible ways to use it is trough docker:
+
+To run redis with docker:
+
+```shell
+docker run --name some-redis -p 6379:3379 -d redis
+```
---
+++ src/index.js
@@ -0,0 +1,83 @@
+const express = require("express");
+const axios = require("axios");
+const { createClient } = require("redis");
+const responseTime = require("response-time");
+
+const app = express();
+
+// Connecting to redis
+const client = createClient({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
+app.use(responseTime());
+
+// Get all characters
+app.get("/character", async (req, res, next) => {
+  try {
+    // Search Data in Redis
+    const reply = await client.get("character");
+
+    // if exists returns from redis and finish with response
+    if (reply) return res.send(JSON.parse(reply));
+
+    // Fetching Data from Rick and Morty API
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/character"
+    );
+
+    // Saving the results in Redis. The "EX" and 10, sets an expiration of 10 Seconds
+    const saveResult = await client.set(
+      "character",
+      JSON.stringify(response.data),
+      {
+        EX: 10,
+      }
+    );
+    console.log(saveResult)
+
+    // respond to client
+    res.send(response.data);
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+// Get a single character
+app.get("/character/:id", async (req, res, next) => {
+  try {
+    const reply = await client.get(req.params.id);
+
+    if (reply) {
+      console.log("using cached data");
+      return res.send(JSON.parse(reply));
+    }
+
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/character/" + req.params.id
+    );
+    const saveResult = await client.set(
+      req.params.id,
+      JSON.stringify(response.data),
+      {
+        EX: 15,
+      }
+    );
+
+    console.log("saved data:", saveResult);
+
+    res.send(response.data);
+  } catch (error) {
+    console.log(error);
+    res.send(error.message);
+  }
+});
+
+async function main() {
+  await client.connect();
+  app.listen(3000);
+  console.log("server listen on port 3000");
+}
+
+main();
